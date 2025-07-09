dc5f9f8777a73e657b1939ba7ed7c2d3303e5b91
Bence Erős <ebence88@gmail.com>
2025-07-09 06:01:27 +0200

    Deployed 6ccd9ec with MkDocs version: 1.5.3

--- src/main/java/com/github/erosb/kappa/core/model/v3/OAI3.java
+++
@@ -1,9 +0,0 @@
-package com.github.erosb.kappa.core.model.v3;
-
-import com.github.erosb.kappa.core.model.OAI;
-
-/**
- * Open API v3 declaration for typing.
- */
-public interface OAI3 extends OAI {
-}
