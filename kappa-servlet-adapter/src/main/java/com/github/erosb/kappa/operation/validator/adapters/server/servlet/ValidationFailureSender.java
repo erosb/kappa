becd7dea69b12114a5ae84936a2d9d3625b91e7a
Bence Erős <ebence88@gmail.com>
2025-07-23 19:00:59 +0200

    implement an RFC9457-compliant ValidationFailureSender

--- kappa-servlet-adapter/src/main/java/com/github/erosb/kappa/operation/validator/adapters/server/servlet/ValidationFailureSender.java
+++ kappa-servlet-adapter/src/main/java/com/github/erosb/kappa/operation/validator/adapters/server/servlet/ValidationFailureSender.java
@@ -6,13 +6,20 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.erosb.kappa.core.validation.OpenApiValidationFailure;
 import com.github.erosb.kappa.core.validation.ValidationException;
 import jakarta.servlet.http.HttpServletResponse;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+
+import static java.util.Objects.requireNonNull;
 
 public interface ValidationFailureSender {
 
   static ValidationFailureSender defaultSender() {
     return new DefaultValidationFailureSender();
+  }
+
+  static ValidationFailureSender rfc9457Sender() {
+    return new RFC9457FailureSender();
   }
 
   void send(ValidationException ex, HttpServletResponse httpResp)
@@ -35,21 +42,7 @@ class DefaultValidationFailureSender
       itemsJson.add(itemJson);
     }
     ex.results().forEach(item -> {
-      ObjectNode itemJson = objectMapper.createObjectNode();
-      itemJson.put("dataLocation", item.describeInstanceLocation());
-      String schemaLocation = item.describeSchemaLocation();
-      int openapiDirIndex = schemaLocation.lastIndexOf("openapi/");
-      if (openapiDirIndex >= 0) {
-        itemJson.put("schemaLocation", schemaLocation.substring(openapiDirIndex));
-      } else {
-        itemJson.put("schemaLocation", schemaLocation);
-      }
-      if (item instanceof OpenApiValidationFailure.SchemaValidationFailure) {
-        OpenApiValidationFailure.SchemaValidationFailure schemaValidationFailure =
-          (OpenApiValidationFailure.SchemaValidationFailure) item;
-        itemJson.put("dynamicPath", schemaValidationFailure.getFailure().getDynamicPath().getPointer().toString());
-      }
-      itemJson.put("message", item.getMessage());
+      ObjectNode itemJson = failureToJson(item, objectMapper);
       itemsJson.add(itemJson);
     });
     respObj.put("errors", itemsJson);
@@ -59,6 +52,59 @@ class DefaultValidationFailureSender
       .writeValueAsString(respObj)
     );
 
+    httpResp.flushBuffer();
+  }
+
+  static ObjectNode failureToJson(OpenApiValidationFailure item, ObjectMapper objectMapper) {
+    ObjectNode itemJson = objectMapper.createObjectNode();
+    itemJson.put("dataLocation", item.describeInstanceLocation());
+    String schemaLocation = item.describeSchemaLocation();
+    int openapiDirIndex = schemaLocation.lastIndexOf("openapi/");
+    if (openapiDirIndex >= 0) {
+      itemJson.put("schemaLocation", schemaLocation.substring(openapiDirIndex));
+    } else {
+      itemJson.put("schemaLocation", schemaLocation);
+    }
+    if (item instanceof OpenApiValidationFailure.SchemaValidationFailure) {
+      OpenApiValidationFailure.SchemaValidationFailure schemaValidationFailure =
+        (OpenApiValidationFailure.SchemaValidationFailure) item;
+      itemJson.put("dynamicPath", schemaValidationFailure.getFailure().getDynamicPath().getPointer().toString());
+    }
+    itemJson.put("message", item.getMessage());
+    return itemJson;
+  }
+}
+
+class RFC9457FailureSender
+  implements ValidationFailureSender {
+
+  private final String typeAttributeValue;
+
+  public RFC9457FailureSender() {
+    this("https://erosb.github.io/kappa/request-validation-failure");
+  }
+
+  public RFC9457FailureSender(String typeAttributeValue) {
+    this.typeAttributeValue = requireNonNull(typeAttributeValue);
+  }
+
+  @Override
+  public void send(ValidationException ex, HttpServletResponse httpResp)
+    throws IOException {
+    ObjectMapper objectMapper = new ObjectMapper();
+    ObjectNode respObj = objectMapper.createObjectNode();
+    ArrayNode errors = objectMapper.createArrayNode();
+    respObj.put("type", typeAttributeValue);
+    respObj.put("status", 400);
+    respObj.put("title", "Validation failure");
+    respObj.put("detail", ex.getMessage());
+    ex.results().forEach(item -> {
+      errors.add(DefaultValidationFailureSender.failureToJson(item, objectMapper));
+    });
+    respObj.put("errors", errors);
+
+    httpResp.setStatus(400);
+    httpResp.getWriter().print(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(respObj));
     httpResp.flushBuffer();
   }
 }
---
+++ kappa-spring/src/test/java/com/github/erosb/kappa/operation/validator/adapters/server/servlet/RFC9457FailureSenderTest.java
@@ -0,0 +1,41 @@
+package com.github.erosb.kappa.operation.validator.adapters.server.servlet;
+
+import com.github.erosb.jsonsKema.JsonPointer;
+import com.github.erosb.jsonsKema.SourceLocation;
+import com.github.erosb.kappa.core.validation.OpenApiValidationFailure;
+import com.github.erosb.kappa.core.validation.ValidationException;
+import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.net.URI;
+import java.util.List;
+
+public class RFC9457FailureSenderTest {
+
+  @Test
+  public void test()
+    throws Exception {
+    var subject = ValidationFailureSender.rfc9457Sender();
+
+    MockHttpServletResponse response = new MockHttpServletResponse();
+    subject.send(new ValidationException("Houston, we have a problem", List.of(OpenApiValidationFailure.unknownStatusCode(418,
+      new SourceLocation(10, 12, new JsonPointer(), new URI("file:///home/erosb/openapi/pets-api.yaml"))))), response);
+
+    var actual = response.getContentAsString();
+
+    JSONAssert.assertEquals("""
+      {
+         "type" : "https://erosb.github.io/kappa/request-validation-failure",
+         "status" : 400,
+         "title" : "Validation failure",
+         "detail" : "Houston, we have a problem",
+         "errors" : [ {
+           "dataLocation" : "$response.status",
+           "schemaLocation" : "openapi/pets-api.yaml",
+           "message" : "Unknown status code 418"
+         } ]
+       }
+      """, actual, true);
+  }
+}
