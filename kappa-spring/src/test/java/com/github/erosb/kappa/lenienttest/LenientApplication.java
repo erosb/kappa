945ba495cde448351f03e67776a5c1472da3dc86
Bence Erős <ebence88@gmail.com>
2026-06-04 20:47:58 +0200

    squash LenientApplication into one file

--- kappa-spring/src/test/java/com/github/erosb/kappa/lenienttest/LenientApplication.java
+++ kappa-spring/src/test/java/com/github/erosb/kappa/lenienttest/LenientApplication.java
@@ -7,6 +7,12 @@ import com.github.erosb.kappa.autoconfigure.KappaSpringConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.LinkedHashMap;
 
@@ -30,5 +36,22 @@ public class LenientApplication {
       .build()
     );
     return kappaConfig;
+  }
+}
+
+class CreateUserRequest {
+  private String name;
+  private String email;
+  private Integer age;
+}
+
+@RestController
+@RequestMapping("/users")
+class UsersController {
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  void createUser(@RequestBody CreateUserRequest request) {
+    System.out.println("received request: " + request);
   }
 }
